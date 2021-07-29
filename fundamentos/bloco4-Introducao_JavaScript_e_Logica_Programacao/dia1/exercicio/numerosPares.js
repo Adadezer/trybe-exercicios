6553a5dd84f68a37e98a340a99700cf3373388a5
Adadezer <adadezer@gmail.com>
2021-07-29 09:04:12 -0300

    feito exercicio 8 - tres numeros pares

---
+++ fundamentos/bloco4-Introducao_JavaScript_e_Logica_Programacao/dia1/exercicio/numerosPares.js
@@ -0,0 +1,9 @@
+let firstNumber = 7;
+let secondNumber = 11;
+let thirdNumber = 3;
+
+if (firstNumber % 2 == 0 || secondNumber % 2 == 0 || thirdNumber % 2 ==  0) {
+    console.log(true);
+}else{
+    console.log(false);
+}
