cb8176168f4b8ab51c1b7f4104c3000bcf8a36b4
Adadezer <adadezer@gmail.com>
2021-08-03 00:10:49 -0300

    4.4 exercicio 5 - fazer um segundo objeto e imprimir os valores dos dois juntos

--- fundamentos/bloco4-Introducao_JavaScript_e_Logica_Programacao/dia4/exercicios/parte1.js
+++ fundamentos/bloco4-Introducao_JavaScript_e_Logica_Programacao/dia4/exercicios/parte1.js
@@ -30,3 +30,21 @@ for (let key in info){
   console.log(info[key]);
 }
 console.log ();
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+console.log ('5:');
+
+let infoPatinhas = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+
+for(key in infoPatinhas){
+  if (key === 'recorrente' && info.recorrente === 'Sim' && infoPatinhas.recorrente === 'Sim'){
+    console.log('Ambos recorrentes');
+  }else{
+    console.log(info[key] + ' e ' + infoPatinhas[key]);
+  }
+}
