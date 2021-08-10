9b04fe80ec572835ad140cd58d5c635ab3f906df
Adadezer <adadezer@gmail.com>
2021-08-10 20:30:47 -0300

    conteudo, criando elementos, exercicio 1 - Crie um irmão para elementoOndeVoceEsta

--- fundamentos/bloco5-javascript_DOM_eventos_webStorage/5.2/conteudos/criandoElementos/scriptCriandoElementos.js
+++ fundamentos/bloco5-javascript_DOM_eventos_webStorage/5.2/conteudos/criandoElementos/scriptCriandoElementos.js
@@ -1,3 +1,11 @@
+// 1- Crie um irmão para elementoOndeVoceEsta
+let pai = document.querySelector("#pai");
+let sectionIrmao = document.createElement("section");
+sectionIrmao.id = "irmaoOndeEstou";
+
+document.body.appendChild(sectionIrmao);
+pai.appendChild(sectionIrmao);
+
 // 2- Crie um filho para elementoOndeVoceEsta .
 let ondeEstou = document.querySelector("#elementoOndeVoceEsta");
 let sectionFilhoOndeEstou = document.createElement("section");
