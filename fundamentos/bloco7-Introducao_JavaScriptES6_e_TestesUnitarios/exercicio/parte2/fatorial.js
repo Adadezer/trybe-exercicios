0bf28b0db588bb7fdc263389e4775c8a3d1ee678
Adadezer <adadezer@gmail.com>
2021-08-28 11:36:23 -0300

    exercicio 1 - fatorial usando arrow function

--- fundamentos/bloco7-Introducao_JavaScriptES6_e_TestesUnitarios/exercicio/parte2/fatorial.js
+++ fundamentos/bloco7-Introducao_JavaScriptES6_e_TestesUnitarios/exercicio/parte2/fatorial.js
@@ -1,14 +1,13 @@
 /* Crie uma função que receba um número e retorne seu fatorial.
 
 Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24. */
-let fatorial = () => {
-    let numero = 6;
+const fatorial = numero => {
     for (let i = numero - 1; i >= 1; i -= 1) {
         numero = numero * i;
     }
     return numero;
 }
-console.log(fatorial());
+console.log(fatorial(5));
 
 /* Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator */
 
