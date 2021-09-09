08d9e27e21648bedee60192ffb993861daceca93
Adadezer <adadezer@gmail.com>
2021-09-09 18:32:12 -0300

    exercicio 1 reduce - concatenar arrays

---
+++ fundamentos/bloco8-Introducao_Higher_Order_Functions/8.4/exercicios/exercise1.js
@@ -0,0 +1,19 @@
+// 1 - Dada uma matriz, transforme em um array.
+
+const assert = require('assert');
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  // escreva seu código aqui
+    return arrays.reduce((acc, curr) =>{
+        return acc.concat(curr);
+    });
+
+}
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
