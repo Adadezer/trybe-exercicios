e1db2fcfef9f848084c845a18ad8509ba27ebfc8
Adadezer <adadezer@gmail.com>
2021-10-04 20:16:46 -0300

    conteudo Introducao React

---
+++ frontEnd/bloco11-IntroducaoReact/11.1/conteudo/conteudo-react_componente-classe/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import ComponentNome from './Component';
+import './App.css';
+
+
+function App() {
+  return (
+    <ComponentNome />
+  );
+}
+
+export default App;
