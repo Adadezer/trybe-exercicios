11a1722954dea5f11761a9628033eeca4184200a
Adadezer <adadezer@gmail.com>
2021-10-12 12:28:08 -0300

    conteudo - handle generico

--- frontEnd/bloco11-Componentes_com_Estado_e_Eventos/11.2/formulario-conteudo-11-2/src/formulario.jsx
+++ frontEnd/bloco11-Componentes_com_Estado_e_Eventos/11.2/formulario-conteudo-11-2/src/formulario.jsx
@@ -6,17 +6,19 @@ class Formulario extends Component {
 
     this.state = {
       tecnologia: 'react',
-      idade: 0,
+      idade: 18,
       conhecimento: false,
       textarea: '',
     }
   }
 
   handleChange = ({target}) => {
-    const {id, value} = target;
+    const {id} = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
     this.setState({
-      [id]: value // eu q escolhi fazer com id, nos exemplos foi feito com nome, o importante é o valor passado na chave setState ser igual a chave state
-    })
+      [id]: value, // eu q escolhi fazer o handle genérico com o id, nos exemplos foi feito com o nome, o importante é o valor passado na chave do this.setState ser igual a chave no this.state
+    });
   }
   /*
   handleTecnologia = (event) => {
@@ -45,33 +47,42 @@ class Formulario extends Component {
         <form className = 'formPalestra'>
 
           <label htmlFor = 'tecnologia'>Tecnologia:
-          <select value = {this.state.tecnologia} onChange = {this.handleChange} id = 'tecnologia'>
-            <option value = 'react'> React </option>
-            <option value = 'javaScript'> Java Script </option>
-            <option value = 'htmlCss'> HTML / CSS </option>
-          </select>
+            <select value = {this.state.tecnologia} onChange = {this.handleChange} id = 'tecnologia'> {/* o value da aqui da tag não interfere no value na função, esse  da tag é só pra setar um valor pré definido quando atualizar a pag*/}
+              <option value = 'react'> React </option>
+              <option value = 'javaScript'> Java Script </option>
+              <option value = 'htmlCss'> HTML / CSS </option>
+            </select>
           </label>
 
           <br /><br />
 
-          <label htmlFor = 'idade'>Idade:</label>
-          <input type = 'number' id = 'idade'
-            value = {this.state.idade} onChange = {this.handleChange}
-          />
+          <fieldset>
+            <legend>informe seus dados:</legend>
 
-          <br /><br />
+            <label htmlFor = 'idade'>Idade: </label>
+            <input type = 'number' id = 'idade'
+              value = {this.state.idade} onChange = {this.handleChange}
+            />
 
-          <label htmlFor = 'conhecimento'> possui conhecimento prévio? </label>
-          <input type = 'checkbox' id = 'conhecimento'/> 
-          
-          <br /><br />
+            <br /><br />
 
-          <label htmlFor = 'textarea'> descreva resumidamente seus conhecimentos</label> <br />
-          <textarea cols = '45' rows = '10' id = 'textarea'
-            value = {this.state.textarea} onChange = {this.handleChange}
-          >
-          </textarea>
-          
+            <label htmlFor = 'conhecimento'> possui conhecimento prévio? </label>
+            <input type = 'checkbox' id = 'conhecimento' value = {this.state.conhecimento} onChange = {this.handleChange}/> 
+            
+            <br /><br />
+
+            <label htmlFor = 'textarea'> descreva resumidamente seus conhecimentos </label> 
+            <br />
+            <textarea cols = '45' rows = '10' id = 'textarea'
+              value = {this.state.textarea} onChange = {this.handleChange}
+            >
+            </textarea>
+
+            <br /><br />
+
+            <legend>mande-nos uma foto sua :)</legend>
+            <input type="file" id="arquivo" />
+          </fieldset>
         </form>
       </div>
     );
