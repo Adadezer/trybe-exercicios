cc10ddb98b2a35e762a760e1502572db8f0886d8
Adadezer <adadezer@gmail.com>
2022-03-05 17:14:49 -0300

    getAllBooks

---
+++ backEnd/bloco24-NodeJS_ORM_e_autenticacao/24.1/exercicios/controllers/bookController.js
@@ -0,0 +1,17 @@
+const { Book } = require('../models');
+
+const getAllBooks = async (_req, res) => {
+  try {
+    const books = await Book.findAll();
+    return res.status(200).json(books);
+
+  } catch (error) {
+    console.log(error.message);
+
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+};
+
+module.exports = {
+  getAllBooks,
+}
