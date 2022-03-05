0c32461994e911354057220865dfb1a34bac9983
Adadezer <adadezer@gmail.com>
2022-03-05 17:26:47 -0300

    getBookById

--- backEnd/bloco24-NodeJS_ORM_e_autenticacao/24.1/exercicios/controllers/bookController.js
+++ backEnd/bloco24-NodeJS_ORM_e_autenticacao/24.1/exercicios/controllers/bookController.js
@@ -2,7 +2,7 @@ const { Book } = require('../models');
 
 const getAllBooks = async (_req, res) => {
   try {
-    const books = await Book.findAll();
+    const books = await Book.findAll(); // método findAll do Sequelize para retornar todos os livros.
     return res.status(200).json(books);
 
   } catch (error) {
@@ -12,6 +12,20 @@ const getAllBooks = async (_req, res) => {
   }
 };
 
+const getBookById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findByPk(id); // método findByPk do Sequelize para buscar o livro pelo id.
+    return res.status(200).json(book);
+
+  } catch (error) {
+    console.log(error.message);
+
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+} 
+
 module.exports = {
   getAllBooks,
+  getBookById,
 }
--- backEnd/bloco24-NodeJS_ORM_e_autenticacao/24.1/exercicios/routerBooks.js
+++ backEnd/bloco24-NodeJS_ORM_e_autenticacao/24.1/exercicios/routerBooks.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const books = require('./controllers/bookController');
 
 router.get('/', books.getAllBooks);
+router.get('/:id', books.getBookById)
 
 module.exports = router;
