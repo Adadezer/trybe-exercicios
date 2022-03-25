39b8e62e5c0e2d470d00f00785218c3807ff5dba
Adadezer <adadezer@gmail.com>
2022-03-25 14:57:37 -0300

    buscando um livro por id

--- backEnd/bloco26-TypeScript/26.3/conteudo/express-typescript/controllers/books.controller.ts
+++ backEnd/bloco26-TypeScript/26.3/conteudo/express-typescript/controllers/books.controller.ts
@@ -7,9 +7,22 @@ import BookService from '../services/books.service';
 
 class BooksController {
   bookService = new BookService();
+
   public async getAll(_req: Request, res: Response) {
     const books = await this.bookService.getAll();
     res.status(StatusCodes.OK).json(books);
+  }
+
+  public async getById(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+    const book = await this.bookService.getById(id);
+
+    if (!book) {
+      return res.status(StatusCodes.NOT_FOUND)
+        .json({ message: 'Book not found!'});
+    }
+
+    res.status(StatusCodes.OK).json(book);
   }
 }
 
@@ -26,6 +39,7 @@ export default BooksController;
 
 // class BooksController {
 //   bookService = new BookService();
+
 //   public getAll = async (_req: Request, res: Response) => {
 //     const books = await this.bookService.getAll();
 //     res.status(StatusCodes.OK).json(books);
