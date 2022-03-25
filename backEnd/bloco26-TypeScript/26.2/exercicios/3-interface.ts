ea5c45d710e57adf4e7ff11cc0843c9793e3ffeb
Adadezer <adadezer@gmail.com>
2022-03-24 23:07:47 -0300

    exercicios 3 e 4 - pizzas

--- backEnd/bloco26-TypeScript/26.2/exercicios/3-interface.ts
+++ backEnd/bloco26-TypeScript/26.2/exercicios/3-interface.ts
@@ -12,9 +12,41 @@
 */
 type Slices = 4 | 6 | 8;
 
-interface Pizza {
+export interface Pizza {
   flavor: string;
   slices: Slices;
 }
 
-export default Pizza
+/* Exercício 4:
+
+  Vamos agora extender nossa interface Pizza com novos tipos de pizza. São eles:
+  Pizza Comum - seus sabores são “Calabresa”, “Frango” e “Pepperoni”, podem ter 4, 6 ou 8 pedaços.
+  Pizza Vegetariana - seus sabores são “Marguerita”, “Palmito” e “Cogumelo”, podem ter 4, 6 ou 8 pedaços.
+  Pizza Doce - seus sabores são “Nutela”, “Goiabada com Queijo” e “Marshmallow”, podem ter somente 4 pedaços.
+  
+  Você deve usar type alias e type unions para criar os possíveis sabores de cada tipo de pizzza.
+  
+  crie:
+  3 pizzas do tipo comum;
+  2 pizzas do tipo vegetariana;
+  1 pizza do tipo doce.
+*/
+
+type Comum =  'Calabresa' | 'Frango' | 'Pepperoni';
+type Vegetariana = 'Marguerita' | 'Palmito' | 'Cogumelo';
+type Doce = 'Nutela' | 'Goiabada com Queijo' | 'Marshmallow';
+
+export interface PizzaComum {
+  flavor: Comum;
+  slices: Slices;
+}
+
+export interface PizzaVegetariana {
+  flavor: Vegetariana;
+  slices: Slices;
+}
+
+export interface PizzaDoce {
+  flavor: Doce;
+  slices: 4;
+}
