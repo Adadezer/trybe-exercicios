c9d4da9faf940989ed2c6a0badcd22bcaf0e553c
Adadezer <adadezer@gmail.com>
2022-03-27 13:01:55 -0300

    rotas

--- backEnd/bloco26-TypeScript/26.3/exercicio/index.ts
+++ backEnd/bloco26-TypeScript/26.3/exercicio/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import RouterUser from './routes/users.routes';
 import 'express-async-errors';
 
 const app = express();
@@ -11,6 +12,8 @@ const PORT = 8000;
 app.get('/', (req: Request, res: Response) => {
     res.status(StatusCodes.OK).send('Express + TypeScript exc')
 });
+
+app.use(RouterUser);
 
 app.use((err: Error , req: Request, res: Response, next: NextFunction) => {
   const { name, message, details } = err as any;
