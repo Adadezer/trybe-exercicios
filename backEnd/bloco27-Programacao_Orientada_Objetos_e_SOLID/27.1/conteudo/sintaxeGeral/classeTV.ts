196f6f2a82f5c57164b944579c2c5a08b1f5280a
Adadezer <adadezer@gmail.com>
2022-04-01 16:24:10 -0300

    sintaxe geral - classe TV

--- backEnd/bloco27-Programacao_Orientada_Objetos_e_SOLID/27.1/conteudo/sintaxeGeral/classeTV.ts
+++ backEnd/bloco27-Programacao_Orientada_Objetos_e_SOLID/27.1/conteudo/sintaxeGeral/classeTV.ts
@@ -1,3 +1,13 @@
+/* Para Fixar
+  Utilizando o que você já aprendeu até aqui, faça os seguintes exercícios:
+  
+  Crie uma classe chamada Tv, com os atributos:
+  - brand : marca da TV;
+  - size : tamanho em polegadas;
+  - resolution : resolução da tela;
+  - connections : conexões disponíveis(HDMI, Ethernet, etc.);
+*/
+
 class TV {
   _brand: string;
   _size: number;
@@ -10,11 +20,12 @@ class TV {
     this._resolution = r;
     this._connections = con;
   }
-
+ // Dentro da classe Tv, crie o método turnOn , que imprimirá os atributos inicializados no construtor.
   turnOn() {
     console.log(`TV ${this._brand}, tamanho: ${this._size} polegadas, resolução: ${this._resolution} pxl, conexões: ${this._connections}`);
   }
 }
 
+// Instancie um objeto a partir da classe Tv, chame o método turnOn para imprimir seus atributos.
 const tv1 = new TV('LG', 29, '4k', ['HDMI', 'Ethernet']);
 tv1.turnOn();
