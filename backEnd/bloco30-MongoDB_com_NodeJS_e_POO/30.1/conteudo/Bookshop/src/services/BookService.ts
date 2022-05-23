e561d8c42fb12ec0a142b0dc8c351bacbff39084
Adadezer <adadezer@gmail.com>
2022-05-23 18:34:30 -0300

    Para Fixar - Listar filmes de outro banco de dados usando o mongoose

--- backEnd/bloco30-MongoDB_com_NodeJS_e_POO/30.1/conteudo/Bookshop/src/services/BookService.ts
+++ backEnd/bloco30-MongoDB_com_NodeJS_e_POO/30.1/conteudo/Bookshop/src/services/BookService.ts
@@ -1,35 +1,49 @@
-  // src/services/BookService.ts
+// src/services/BookService.ts
 
-  import BookModel from '../models/BookModel';
-  import { IBook } from '../schemas/BookSchema';
+import { BookModel, MovieModel } from '../models/BookModel';
+import { IBook } from '../schemas/BookSchema';
+import { IMovie } from '../schemas/MovieSchema';
 
-  class BookService {
-    constructor(private bookModel = new BookModel()) {}
+class BookService {
+  constructor(private bookModel = new BookModel()) {}
 
-    public async getBooks(): Promise<IBook[]> {
-      const books = await this.bookModel.getBooks();
-      return books;
-    }
-
-    // public async createBook(bookData: object): Promise<IBook> {
-    //   const book = await this.bookModel.createBook(bookData);
-    //   return book;
-    // }
-
-    // public async getBook(id: string): Promise<IBook | null> {
-    //   const data = await this.bookModel.getBook(id);
-    //   return data;
-    // }
-
-    // public async updateBook(id: string, bookData: object): Promise<IBook | null> {
-    //   const data = await this.bookModel.editBook(id, bookData);
-    //   return data;
-    // }
-
-    // public async deleteBook(id: string): Promise<IBook | null> {
-    //   const data = await this.bookModel.deleteBook(id);
-    //   return data;
-    // }
+  public async getBooks(): Promise<IBook[]> {
+    const books = await this.bookModel.getBooks();
+    return books;
   }
 
-  export default BookService;
+  // public async createBook(bookData: object): Promise<IBook> {
+  //   const book = await this.bookModel.createBook(bookData);
+  //   return book;
+  // }
+
+  // public async getBook(id: string): Promise<IBook | null> {
+  //   const data = await this.bookModel.getBook(id);
+  //   return data;
+  // }
+
+  // public async updateBook(id: string, bookData: object): Promise<IBook | null> {
+  //   const data = await this.bookModel.editBook(id, bookData);
+  //   return data;
+  // }
+
+  // public async deleteBook(id: string): Promise<IBook | null> {
+  //   const data = await this.bookModel.deleteBook(id);
+  //   return data;
+  // }
+}
+
+// Para Fixar
+class MovieService {
+  constructor( private movieModel = new MovieModel()) {}
+
+    public async getAll(): Promise<IMovie[]> {
+    const movies = await this.movieModel.getAll();
+    return movies;
+  }
+}
+
+export {
+  BookService,
+  MovieService
+};
