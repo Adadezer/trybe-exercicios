48bbdac291342253d2adf13c4e8cb434c37165e9
Adadezer <adadezer@gmail.com>
2022-07-22 11:23:04 -0300

    conteudo do dia, e exercicios de fixação

--- cienciaComputacao/bloco36-Algoritmos/36.1/conteudo/complexidade_quadratica/exc_fixacao3.py
+++ cienciaComputacao/bloco36-Algoritmos/36.1/conteudo/complexidade_quadratica/exc_fixacao3.py
@@ -24,3 +24,7 @@ cubic_function(my_array, my_array, my_array)
 
 # comando para ver tempo de execução:
 # time python3 meu_programa.py
+
+# A complexidade será O(n³). Em termos do número de iterações necessárias, para um array de 10 elementos iremos realizar 1000 iterações! 101010 == 1000. O tempo de execução pode variar entre uma máquina e outra, mas, se estiver no ambiente Linux ou MacOS, tente utilizar o comando time para aferir o tempo de execução para entradas variáveis em seu computador! Observe o padrão de crescimento.
+
+# Dica: Tenha sempre o ctrl+c engatilhado em seu terminal para encerrar o processo caso este tome muito tempo para concluir.
